4123cc5d2b030114fa96449d4ca0a102b3f053a8
zivattias <ziv.attias7@gmail.com>
2024-01-05 16:04:43 +0200

    Add graceful shutdown

--- application/app.go
+++ application/app.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -29,16 +30,37 @@ func (a *App) connectToDb(ctx context.Context) error {
 	return nil
 }
 
-func (a *App) startServer() error {
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: a.router,
-	}
-
-	if err := server.ListenAndServe(); err != nil {
-		return fmt.Errorf("failed to start server: %w", err)
+func (a *App) closeDb() error {
+	if err := a.db.Close(); err != nil {
+		return fmt.Errorf("failed to close redis: %w", err)
 	}
 	return nil
+}
+
+func (a *App) startServer(ctx context.Context, server *http.Server) error {
+	ch := make(chan error, 1)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			ch <- fmt.Errorf("failed to start server: %w", err)
+		}
+		close(ch)
+	}()
+
+	select {
+	case err := <-ch:
+		return err
+	case <-ctx.Done():
+		timeout, cancel := context.WithTimeout(context.Background(), time.Second*10)
+		defer cancel()
+
+		if shutdownErr := server.Shutdown(timeout); shutdownErr != nil {
+			return fmt.Errorf("failed to shutdown server: %w", shutdownErr)
+		}
+
+		return nil
+	}
 }
 
 func (a *App) Start(ctx context.Context) error {
@@ -46,9 +68,14 @@ func (a *App) Start(ctx context.Context) error {
 		return err
 	}
 
-	if err := a.startServer(); err != nil {
-		return err
+	defer a.closeDb()
+
+	fmt.Println("Starting server")
+
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: a.router,
 	}
 
-	return nil
+	return a.startServer(ctx, server)
 }
