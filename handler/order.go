7697453c49bc7c633c06e1ff6ef367b6c245d42c
zivattias <ziv.attias7@gmail.com>
2024-01-06 15:46:47 +0200

    Add model handler and Python script for generating models in db

--- handler/order.go
+++ handler/order.go
@@ -1,18 +1,102 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/zivattias/golang-redis-microservice/model"
+	"github.com/zivattias/golang-redis-microservice/repository/order"
 )
 
-type Order struct{}
+type Order struct {
+	Repo *order.RedisRepo
+}
 
 func (o *Order) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Create an order")
+	var body struct {
+		CustomerID uuid.UUID        `json:"customer_id,omitempty"`
+		LineItems  []model.LineItem `json:"line_items,omitempty"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	now := time.Now().UTC()
+
+	order := model.Order{
+		OrderID:    rand.Uint64(),
+		CustomerID: body.CustomerID,
+		LineItems:  body.LineItems,
+		CreatedAt:  &now,
+	}
+
+	err := o.Repo.Insert(r.Context(), order)
+	if err != nil {
+		fmt.Println("failed to insert:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	res, err := json.Marshal(order)
+	if err != nil {
+		fmt.Println("failed to marshal order:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(res)
+	w.WriteHeader(http.StatusCreated)
 }
 
 func (o *Order) List(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("List all orders")
+	cursorStr := r.URL.Query().Get("cursor")
+	if cursorStr == "" {
+		cursorStr = "0"
+	}
+
+	const decimal = 10
+	const bitSize = 64
+	cursor, err := strconv.ParseUint(cursorStr, decimal, bitSize)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	const size = 50
+	res, err := o.Repo.FindAll(r.Context(), model.FindAllPage{
+		Offset: cursor,
+		Size:   size,
+	})
+
+	if err != nil {
+		fmt.Println("failed to find all:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var response struct {
+		Items []model.Order `json:"items"`
+		Next  uint64        `json:"next"`
+	}
+
+	response.Items = res.Orders
+	response.Next = res.Cursor
+
+	data, err := json.Marshal(response)
+	if err != nil {
+		fmt.Println("failed to marshal:", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(data)
 }
 
 func (o *Order) GetById(w http.ResponseWriter, r *http.Request) {
